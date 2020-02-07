b0cff85430321e7ba049379e6188a48247837766
Albert Lam <albert233@hotmail.com>
2020-02-07 17:35:38 -0600

    Rewrote P02.hs and P04.hs

--- P02.hs
+++ P02.hs
@@ -1,3 +1,8 @@
 -- Find the last but one element of a list.
 
-myButLast x = last (init x)
+myButLast :: [a] -> a
+myButLast [] = error "There is no last but one element in an empty list."
+myButLast [_] = error "There is no last but one element in a singleton list."
+myButLast (x:_:[]) = x
+myButLast (_:xs) = myButLast xs
+
--- P04.hs
+++ P04.hs
@@ -1,3 +1,5 @@
 -- Find the number of elements of a list.
 
-myLength x = length x
+myLength :: (Num b) => [a] -> b
+myLength [] = 0
+myLength (_:xs) = 1 + myLength xs
